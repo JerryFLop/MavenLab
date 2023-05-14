afd1fa8f3e2052e2b83cdbbc955d4bab98866ad9
JerryFLop <JParedes@wil.yearup.org>
2023-05-14 18:47:42 -0400

    Commit

---
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -0,0 +1,11 @@
+package io.zipcoder.interfaces;
+
+public class Person {
+    private final long id;
+    private String name ;
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
