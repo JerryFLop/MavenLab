906d97739a46526922d7f32697f9caa8a219ed78
JerryFlop <JParedes@wil.yearup.org>
2023-05-14 16:59:46 -0400

    Commit

---
+++ src/main/java/LeonsLengthyLearnerLab/Person.java
@@ -0,0 +1,18 @@
+package LeonsLengthyLearnerLab;
+
+public class Person {
+
+private final long id;
+private String name ;
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+
+
+
+
+}
